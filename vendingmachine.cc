967beecb99acf777841f9eb49e80fdb9c80a80d6
Frank Li <lxf20035@msn.com>
2013-11-30 15:37:14 -0500

    started vending maching

--- vendingmachine.cc
+++ vendingmachine.cc
@@ -1,12 +1,17 @@
 #include "vendingmachine.h"
 
 void VendingMachine::main() {
-
+    this->mPrt.print(Printer::Vending, this->mId, 'S', this->mSodaCost);
 }
 
 VendingMachine::VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
                     unsigned int maxStockPerFlavour )
-    : mPrt(prt), mNameServer(nameServer), mId(id), mSodaCost(sodaCost), mMaxStockPerFlavour(maxStockPerFlavour) {}
+    : mPrt(prt), mNameServer(nameServer), mId(id), mSodaCost(sodaCost), mMaxStockPerFlavour(maxStockPerFlavour), mRestocking(false) {
+        this->mNameServer.VMregister(this);
+        for (unsigned int i = 0; i < 4; ++i) {
+            this->mSodaStock[i] = 0;
+        }
+    }
 
 VendingMachine::~VendingMachine() {
 
@@ -25,9 +30,9 @@ void VendingMachine::restocked() {
 }
 
 unsigned int VendingMachine::cost() {
-
+    return this->mSodaCost;
 }
 
 unsigned int VendingMachine::getId() {
-
+    return this->mId;
 }
