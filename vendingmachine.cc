4d78f1533b2d5983a3251731f377c6590c1d7c4e
yongxin1029 <yongxin1029@gmail.com>
2013-12-01 19:06:58 -0500

    Update vendingmachine.cc
    linux032 is dead commenting from github.com

--- vendingmachine.cc
+++ vendingmachine.cc
@@ -1,40 +1,9 @@
 #include "vendingmachine.h"
 
-
-
-/*
-A vending machine's function is to sell soda to students at so me cost. 
-
-Each vending machine is passed an id in the range [0, NumVendingMachines) for identification, MSRP price for a bottle of soda, 
-and the maximum number of bottles of each flavor in a vending machine. A new vending machineis empty (nostock) and begins
-by registering with the name server. 
-
-A student calls buy to obtain one of their favorite sodas. If the specified soda is unavailable => STOCK
-or 
-the student has insufficient funds to purchase the soda, buy returns =>FUNDS 
-otherwise, 
-the student's WATCard is debited by the cost of a soda and buy returns BUY.
-
-
-Periodically, the truck comes by to restock the vending machines with new soda from the bottling plant. Re-
-stocking is performed in two steps. The truck calls inventory to return a pointer to an array containing the amount
-of each kind of soda currently in the vending machine. 
-The truck uses this information to transfer into each machine as much of its stock of new soda as fits; 
-for each kind of so da, no more than MaxStockPerFlavour per flavor can be added to a machine. 
-If the truck cannot top-up a particular flavour, it transfers as many bottles as it has (which could be 0). 
-After transferring new soda into the machine by directly modifying the array passed
-from inventory , the truck calls restocked to indicate the operation is complete. 
-
-
-The vending machine cannot
-accept buy calls during restocking. The cost member returns the cost of purchasing a soda for this machine . The
-getIdmember returns the identification number of the vending machine. You define the public type Flavors to
-represent the differentflavours of soda.
-*/
-
-
-
-
+/***************** vendingmachine::main ****************
+ Purpose:   since vending machine is a Task it has to have a main
+ return:  void
+ ******************************************************/
 void VendingMachine::main() {
     mPrt.print(Printer::Vending, mId, 'S', mSodaCost);
     mNameServer.VMregister(this);
@@ -52,6 +21,11 @@ void VendingMachine::main() {
 
 }
 
+
+
+/***************** VendingMachine::VendingMachine ****************
+ Purpose:   constructor 
+ ******************************************************/
 VendingMachine::VendingMachine( Printer &prt, NameServer &nameServer, unsigned int id, unsigned int sodaCost,
                     unsigned int maxStockPerFlavour )
     : mPrt(prt), 
@@ -65,10 +39,20 @@ VendingMachine::VendingMachine( Printer &prt, NameServer &nameServer, unsigned i
       }
 }
 
+
+/***************** VendingMachine::~VendingMachine ****************
+ Purpose:   destructor
+ ******************************************************/
 VendingMachine::~VendingMachine() {
   mPrt.print(Printer::Vending, mId, 'F' );
   delete[] mStocksLevel;
 }
+
+
+/*****************  vendingmachine::buy ****************
+ Purpose:   student buys soda from 
+ return:  the status of the purchase (vendingmachine::Status)
+ ******************************************************/
 
 VendingMachine::Status VendingMachine::buy( Flavours flavour, WATCard &card ) {
   if( mStocksLevel[ flavour ] <= 0 ) {
@@ -82,16 +66,35 @@ VendingMachine::Status VendingMachine::buy( Flavours flavour, WATCard &card ) {
   return BUY;
 }
 
+
+/***************** VendingMachine::inventory ****************
+ Purpose:   request the inventory of the current vending machine
+ return:   the inventory (unsigned int*)
+ ******************************************************/
 unsigned int * VendingMachine::inventory() {
   return mStocksLevel;
 }
 
-void VendingMachine::restocked() {}
+/***************** vendingmachine::restocked ****************
+ Purpose:   tell vending it has been restocked
+ return:  void
+ ******************************************************/
+void VendingMachine::restocked() {
+}
 
+
+/***************** vendingmachine::cost ****************
+ Purpose:   get the price of soda in vending machine
+ return:  the price (unsigned int)
+ ******************************************************/
 _Nomutex unsigned int VendingMachine::cost() {
   return mSodaCost;
 }
 
+/***************** VendingMachine::getId ****************
+ Purpose:   get the id of current vending machine
+ return:  the vending machine id (unsigned int)
+ ******************************************************/
 _Nomutex unsigned int VendingMachine::getId() {
   return mId;
 }
