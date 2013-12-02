6376843ca404e3cae947f7c2b9b875fd5373b7ab
yongxin1029 <yongxin1029@gmail.com>
2013-12-01 19:43:54 -0500

    Update printer.cc
    updated printer

--- printer.cc
+++ printer.cc
@@ -1,7 +1,9 @@
 #include "printer.h"
 
+/***************** Printer::main ****************
+ Purpose:   since this is a comonitor it has to have main
+ ******************************************************/
 void Printer::main() {
-        
   _printHeader();
   for(;;){
     if (!_canInsertData()) {
@@ -18,6 +20,11 @@ void Printer::main() {
   }
 }
 
+
+/***************** Printer::_printHeader ****************
+ Purpose:   printer the header of program
+ return:  void
+ ******************************************************/
 void Printer::_printHeader(){
   cout<<"Parent\tWATOff\tNames\tTruck\tPlant";
   for (unsigned int i = 0; i < mNumStudents; ++i) {
@@ -39,59 +46,69 @@ void Printer::_printHeader(){
 }
 
 
-void Printer::_flashMap(bool isDotFlash){
-  for (unsigned int k = 0; k < (3 + 5); ++k) {
-    if (mDataMap.find(k) != mDataMap.end()) {
+/***************** Printer::_upperRangTranslation ****************
+ Purpose:   translatable how many time need to print for each Kind
+ return:  number of time (unsigned int)
+ ******************************************************/
+unsigned int Printer::_upperRangTranslation(unsigned int kind){
+  switch (k){
+    case ((unsigned int) Printer::Student):
+      return mNumStudents;
+    case ((unsigned int) Printer::Vending):
+      return mNumVendingMachines;
+    case ((unsigned int) Printer::Courier):
+      return mNumCouriers;
+    default:
+      return 1;
+  }
+}
 
+/***************** Printer::_flashMap ****************
+ Purpose:   print the contain of map and clears it
+ return:  void
+ ******************************************************/
+void Printer::_flashMap(bool isDotFlash){
+  // goes though 8 kind of value
+  for (unsigned int k = 0; k < (3 + 5); ++k) {
+    // if the value exist print its value, otherwise print a tab
+    if (mDataMap.find(k) != mDataMap.end()) {
+      // if k is in upper range of the enum, which mean there could be multiple instance of the value
       if (k == ((unsigned int) Printer::Student) || 
           k == ((unsigned int) Printer::Vending) || 
           k == ((unsigned int) Printer::Courier) ) {
-        unsigned int times;
-        switch (k){
-
-
-          case ((unsigned int) Printer::Student):
-          {
-            times = mNumStudents;
-          }
-          break;
-          case ((unsigned int) Printer::Vending):
-          {
-            times = mNumVendingMachines;
-          }
-          break;
-          case ((unsigned int) Printer::Courier):
-          {
-            times = mNumCouriers;
-          }
-          break;
-          default:
-          {
-            assert (false);
-          }
-        }
+        // for the kind k find how many of those possible value could exits
+        unsigned int times = _upperRangTranslation(k);
+        // print that many kind 
         for (int i = 0; i < times; ++i) {
           if (mDataMap[k].find(i) != mDataMap[k].end()){
+            // output the char
             cout << mDataMap[k][i].first;
             if (mDataMap[k][i].second.first != ((unsigned int)-1)) {
+              // first number
               cout << mDataMap[k][i].second.first;
               if (mDataMap[k][i].second.second != ((unsigned int)-1)) {
+                // second number
                 cout << "," <<mDataMap[k][i].second.second;
               }
             }
           } else if (isDotFlash) {
+            // if it is a dot flash
             cout << "...";
           }
           cout << "\t";
         }
       } else {
+        // other wise it in lower range and should only have one instance
         unsigned int i = 0;
         if (mDataMap[k].find(i) != mDataMap[k].end()){
+          // output the char
           cout << mDataMap[k][i].first;
           if (mDataMap[k][i].second.first != ((unsigned int)-1)) {
+            // first number
             cout << mDataMap[k][i].second.first;
             if (mDataMap[k][i].second.second != ((unsigned int)-1))
             {
+              // second number
               cout << "," <<mDataMap[k][i].second.second;
             }
           }
@@ -102,31 +119,11 @@ void Printer::_flashMap(bool isDotFlash){
       }
 
     } else if (isDotFlash) {
+      // if nothing is found should either print dots or a tab
       if (k == ((unsigned int) Printer::Student) || 
           k == ((unsigned int) Printer::Vending) || 
           k == ((unsigned int) Printer::Courier) ) {
-        unsigned int times;
-        switch (k){
-          case ((unsigned int) Printer::Student):
-          {
-            times = mNumStudents;
-          }
-          break;
-          case ((unsigned int) Printer::Vending):
-          {
-            times = mNumVendingMachines;
-          }
-          break;
-          case ((unsigned int) Printer::Courier):
-          {
-            times = mNumCouriers;
-          }
-          break;
-          default:
-          {
-            assert (false);
-          }
-        }
+        unsigned int times = _upperRangTranslation(k);
         for (int i = 0; i < times; ++i) {
           cout << "...";
           cout << "\t";
@@ -136,31 +133,11 @@ void Printer::_flashMap(bool isDotFlash){
         cout << "\t";
       }
     }else {
+      // if nothing is found should either print dots or a tab
       if (k == ((unsigned int) Printer::Student) || 
           k == ((unsigned int) Printer::Vending) || 
           k == ((unsigned int) Printer::Courier) ) {
-        unsigned int times;
-        switch (k){
-          case ((unsigned int) Printer::Student):
-          {
-            times = mNumStudents;
-          }
-          break;
-          case ((unsigned int) Printer::Vending):
-          {
-            times = mNumVendingMachines;
-          }
-          break;
-          case ((unsigned int) Printer::Courier):
-          {
-            times = mNumCouriers;
-          }
-          break;
-          default:
-          {
-            assert (false);
-          }
-        }
+        unsigned int times = _upperRangTranslation (k);
         for (int i = 0; i < times; ++i) {
           cout << "\t";
         }
@@ -175,7 +152,10 @@ void Printer::_flashMap(bool isDotFlash){
 }
 
 
-
+/***************** Printer::_insertData ****************
+ Purpose:   insert data into the map
+ return:  void
+ ******************************************************/
 void Printer::_insertData(){
 
 
@@ -201,11 +181,13 @@ void Printer::_insertData(){
 
 
   if (mDataMap.find(mKind) == mDataMap.end()) {
+    // if the kind entry doesnt exist in the map
     mDataMap[mKind] = std::map<unsigned int, pair<char, 
                             pair<unsigned int , unsigned int > 
                                                   > 
                               > ();
   }
+  // if there isnt a idea for it create one
   if (mDataMap[mKind].find(mLid) == mDataMap[mKind].end()) {
     mDataMap[mKind][mLid] = pair<char, 
                                 pair <unsigned int , unsigned int > > (
@@ -216,10 +198,16 @@ void Printer::_insertData(){
                                   );
                   
   } else {
+    // some thing went really wrong.
     assert(false);
   }
 }
 
+
+/***************** Printer::_canInsertData ****************
+ Purpose:   if with current number can be inserted into the map
+ return:  result (bool)
+ ******************************************************/
 bool Printer::_canInsertData(){
   if (!mDataMap.empty()) {
     if (mDataMap.find(mKind) != mDataMap.end()) {
@@ -236,7 +224,9 @@ bool Printer::_canInsertData(){
 }
 
 
-
+/***************** Printer::Printer ****************
+ Purpose:   the constructor
+ ******************************************************/
 Printer::Printer( unsigned int numStudents, unsigned int numVendingMachines, unsigned int numCouriers )
   : mNumStudents(numStudents), mNumVendingMachines(numVendingMachines),
   mNumCouriers(numCouriers) {
@@ -244,6 +234,9 @@ Printer::Printer( unsigned int numStudents, unsigned int numVendingMachines, uns
 }
 
 
+/***************** Printer::Printer ****************
+ Purpose:   the destructor
+ ******************************************************/
 Printer::~Printer(){
   if (!mDataMap.empty()) {
     _flashMap();
@@ -251,7 +244,9 @@ Printer::~Printer(){
   cout << "***********************" << endl;
 }
 
-
+/***************** Printer::print ****************
+ Purpose:   print to the printer with given arguments
+ ******************************************************/
 void Printer::print( Kind kind, char state ) {
   mKind = static_cast<unsigned int>(kind);
   mState = state;
@@ -264,6 +259,9 @@ void Printer::print( Kind kind, char state ) {
 
 
 
+/***************** Printer::print ****************
+ Purpose:   print to the printer with given arguments
+ ******************************************************/
 void Printer::print( Kind kind, char state, int value1 ) {
   mKind = static_cast<unsigned int>(kind);
   mState = state;
@@ -276,6 +274,9 @@ void Printer::print( Kind kind, char state, int value1 ) {
 
 
 
+/***************** Printer::print ****************
+ Purpose:   print to the printer with given arguments
+ ******************************************************/
 void Printer::print( Kind kind, char state, int value1, int value2 ) {
   mKind = static_cast<unsigned int>(kind);
   mLid = 0;
@@ -288,6 +289,9 @@ void Printer::print( Kind kind, char state, int value1, int value2 ) {
 
 
 
+/***************** Printer::print ****************
+ Purpose:   print to the printer with given arguments
+ ******************************************************/
 void Printer::print( Kind kind, unsigned int lid, char state ) {
   mKind = static_cast<unsigned int>(kind);
   mLid = lid;
@@ -300,6 +304,9 @@ void Printer::print( Kind kind, unsigned int lid, char state ) {
 
 
 
+/***************** Printer::print ****************
+ Purpose:   print to the printer with given arguments
+ ******************************************************/
 void Printer::print( Kind kind, unsigned int lid, char state, int value1 ) {
   mKind = static_cast<unsigned int>(kind);
   mLid = lid;
@@ -313,6 +320,9 @@ void Printer::print( Kind kind, unsigned int lid, char state, int value1 ) {
 
 
 
+/***************** Printer::print ****************
+ Purpose:   print to the printer with given arguments
+ ******************************************************/
 void Printer::print( Kind kind, unsigned int lid, char state, int value1, int value2 ) {
   mKind = static_cast<unsigned int>(kind);
   mLid = lid;
